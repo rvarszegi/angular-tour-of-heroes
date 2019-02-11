4b1add6d26bce533c583ccc5a83465215c7b0249
rvarszegi <r.varszegi93@gmail.com>
2019-02-11 23:22:37 +0100

    asd

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,20 +4,27 @@ import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to web api
+  private handleError<T> (operation = 'operation', result?: T) {
+     return (error: any): Observable<T> => {
+       console.log('${operation} failed: {$error.message}');
+       return of(result as T);
+     };
+   }
    private log (message: string) {
     this.messageService.add('HeroService: ${message}');
   }
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError('getHeroes', [])));
   }
   getHero(id: number): Observable<Hero> {
-    this.messageService.add('HeroService: fetched hero: id=${id}');
+    this.messageService.add('HeroService: fetched hero id=${id}');
     return of (HEROES.find(hero => hero.id === id));
   }
   constructor (
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -20,7 +20,7 @@ export class InMemoryDataService  implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return heroes;
+    return {heroes};
   }
   genId(heroes: Hero[]): number {
       return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
